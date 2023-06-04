24a93db126687384ea8747b2945b61c11af9056f
re150 <a21110175@live.ceti.mx>
2023-06-04 15:22:58 -0600

    Chat privado 3.5

--- Main/src/main/MiclassRemotaPunto.java
+++ Main/src/main/MiclassRemotaPunto.java
@@ -15,21 +15,30 @@ import java.rmi.server.UnicastRemoteObject;
 public class MiclassRemotaPunto extends UnicastRemoteObject implements MiInterfazRemotaPrivada {
 
     public InterfazCliente client;
-   
+    private InterfazCliente cliente2;
     
     public MiclassRemotaPunto () throws RemoteException {
         client = null;
+        cliente2 = null;
     }
     
    public void registerClient(InterfazCliente client1) throws RemoteException{
       System.out.println(client);
-      client = client1;
+      if(client == null){
+          client = client1;
+      }else if(cliente2 == null){
+          cliente2 = client1;
+      } else {
+           System.out.println("No se pueden registrar más clientes");
+      }
+      
     }
    
    public void SendMS(String mensaje ) throws RemoteException {
           System.out.println(mensaje);
            if (client != null) {
             client.recibirMS(mensaje);
+            cliente2.recibirMS(mensaje);
         } else {
             System.out.println("No hay cliente destinatario registrado.");
         }
