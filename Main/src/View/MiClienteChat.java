bd219020d7f93b5169a60dbfbc4b498ccc5f2ec2
re150 <a21110175@live.ceti.mx>
2023-06-05 13:34:15 -0600

    proyecto chat 3.5

--- Main/src/View/MiClienteChat.java
+++ Main/src/View/MiClienteChat.java
@@ -6,6 +6,7 @@ package View;
 
 import Interfaces.MiInterfazRemota;
 import Interfaces.MiInterfazRemotaPrivada;
+import java.awt.Color;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.Naming;
@@ -42,7 +43,7 @@ public class MiClienteChat extends javax.swing.JFrame {
             Registry registry = LocateRegistry.createRegistry(1234);
             registry.rebind("MiInterfazRemotaPrivada", remoteMiPri);
 
-            System.out.println("Servidor RMI listo...");
+            System.out.println("Servidor RMI Privado listo");
         } catch (Exception e) {
             System.err.println("Error  Registry in  servidor: " + e.toString());
             e.printStackTrace();
@@ -59,7 +60,7 @@ public class MiClienteChat extends javax.swing.JFrame {
             registryPrivate = LocateRegistry.getRegistry("192.168.100.5", 1234);
             remoteMiPri = (MiInterfazRemotaPrivada) registryPrivate.lookup("MiInterfazRemotaPrivada");
             
-           ClienteMS cliente = new ClienteMS(ChatP1);
+            ClienteMS cliente = new ClienteMS(ChatP1);
             remoteMiPri.registerClient(cliente);
             // System.out.println(remoteMiaux);
             System.out.println("Conexion exitosa");
@@ -74,26 +75,29 @@ public class MiClienteChat extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        ChatGeneralArea = new javax.swing.JTextArea();
+        jPanel2 = new javax.swing.JPanel();
         BtnGeneral = new javax.swing.JButton();
         ChatGeneral = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        ChatGeneralArea = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
         jScrollPane2 = new javax.swing.JScrollPane();
         ChatP1 = new javax.swing.JTextArea();
+        jLabel5 = new javax.swing.JLabel();
+        Registrarce = new javax.swing.JButton();
+        BtnConexion = new javax.swing.JButton();
         ChatP1Send = new javax.swing.JTextField();
         BtnSP1 = new javax.swing.JButton();
-        BtnConexion = new javax.swing.JButton();
-        Registrarce = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        ChatGeneralArea.setColumns(20);
-        ChatGeneralArea.setRows(5);
-        jScrollPane1.setViewportView(ChatGeneralArea);
-
-        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 60, 313, 301));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         BtnGeneral.setText("Send");
         BtnGeneral.addActionListener(new java.awt.event.ActionListener() {
@@ -101,31 +105,46 @@ public class MiClienteChat extends javax.swing.JFrame {
                 BtnGeneralActionPerformed(evt);
             }
         });
-        jPanel1.add(BtnGeneral, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 380, 57, -1));
-        jPanel1.add(ChatGeneral, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 380, 250, -1));
+        jPanel2.add(BtnGeneral, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 460, 57, -1));
+        jPanel2.add(ChatGeneral, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 460, 250, -1));
+
+        ChatGeneralArea.setColumns(20);
+        ChatGeneralArea.setRows(5);
+        jScrollPane1.setViewportView(ChatGeneralArea);
+
+        jPanel2.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, 313, 301));
+
+        jLabel1.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel1.setFont(new java.awt.Font("Dubai", 3, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Chat General");
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 80, 160, -1));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/pngwing.com (6).png"))); // NOI18N
+        jLabel2.setText("jLabel2");
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, 430, 550));
+
+        jLabel4.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel4.setFont(new java.awt.Font("Dubai", 3, 24)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Chat General");
+        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 80, 160, -1));
+
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 450, 570));
+
+        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         ChatP1.setColumns(20);
         ChatP1.setRows(5);
         jScrollPane2.setViewportView(ChatP1);
 
-        jPanel1.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 20, 327, 150));
-        jPanel1.add(ChatP1Send, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 180, 270, -1));
+        jPanel3.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 160, 320, 220));
 
-        BtnSP1.setText("Send");
-        BtnSP1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BtnSP1ActionPerformed(evt);
-            }
-        });
-        jPanel1.add(BtnSP1, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 180, 60, 30));
-
-        BtnConexion.setText("Conecta");
-        BtnConexion.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BtnConexionActionPerformed(evt);
-            }
-        });
-        jPanel1.add(BtnConexion, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 40, -1, -1));
+        jLabel5.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel5.setFont(new java.awt.Font("Dubai", 3, 24)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("Chat Privado");
+        jPanel3.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 80, 160, -1));
 
         Registrarce.setText("Registra");
         Registrarce.addActionListener(new java.awt.event.ActionListener() {
@@ -133,20 +152,39 @@ public class MiClienteChat extends javax.swing.JFrame {
                 RegistrarceActionPerformed(evt);
             }
         });
-        jPanel1.add(Registrarce, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 90, -1, -1));
+        jPanel3.add(Registrarce, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 270, -1, -1));
+
+        BtnConexion.setText("Conecta");
+        BtnConexion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnConexionActionPerformed(evt);
+            }
+        });
+        jPanel3.add(BtnConexion, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 220, -1, -1));
+        jPanel3.add(ChatP1Send, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 430, 270, -1));
+
+        BtnSP1.setText("Send");
+        BtnSP1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtnSP1ActionPerformed(evt);
+            }
+        });
+        jPanel3.add(BtnSP1, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 430, 60, 30));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/pngwing.com (7).png"))); // NOI18N
+        jPanel3.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 0, 450, 570));
+
+        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 0, 470, 570));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 887, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 899, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 569, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -268,7 +306,14 @@ public class MiClienteChat extends javax.swing.JFrame {
     private javax.swing.JTextArea ChatP1;
     private javax.swing.JTextField ChatP1Send;
     private javax.swing.JButton Registrarce;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     // End of variables declaration//GEN-END:variables
